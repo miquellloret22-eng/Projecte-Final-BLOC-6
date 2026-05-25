2464fe2796af0f8572fb12e321f81206a4048ec3
miquel <miquel.lloret22@lacetania.cat>
2026-05-25 17:09:32 +0200

    projecte acabat

--- blog/models.py
+++ blog/models.py
@@ -1,26 +1,26 @@
-from django.core.validators import FileExtensionValidator
+from django.core.validators import MinLengthValidator, FileExtensionValidator
 from django.db import models
 
 # Create your models here.
 
 class Author(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50, validators=[MinLengthValidator(2)])
+    last_name = models.CharField(max_length=50, validators=[MinLengthValidator(2)])
     email_address = models.EmailField(unique=True)
     
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
 class Tag(models.Model):
-    caption = models.CharField(max_length=50)
+    caption = models.CharField(max_length=50, validators=[MinLengthValidator(2)])
     
     def __str__(self):
         return f"{self.caption}"
 
 class Post(models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=50, validators=[MinLengthValidator(2)])
     excerpt = models.TextField()
-    image_name = models.CharField(max_length=50)
+    image_name = models.CharField(max_length=50, validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png'])])
     date = models.DateField()
     slug = models.SlugField(unique=True, blank=True, null=False, db_index=True)
     content = models.TextField()
